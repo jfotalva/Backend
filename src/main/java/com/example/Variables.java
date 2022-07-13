9f51e150e84d2b3171cfe6b380ca81f0ed4c3022
Johan Otalvaro <jfotalva@gmail.com>
2022-07-13 14:25:03 -0500

    Push desde mi PC

--- src/main/java/com/example/Variables.java
+++ src/main/java/com/example/Variables.java
@@ -3,5 +3,6 @@ package com.example;
 class Variables {
     public static void main(String[] args) {
         System.out.println("Variables.java ...");  
+        System.out.println("Este es un comentario de prueba");  
     }
 }
